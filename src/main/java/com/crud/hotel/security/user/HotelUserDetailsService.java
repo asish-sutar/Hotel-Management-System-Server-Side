a4ed341c279816c67d7109dfe97afe065ca66688
Asish Sutar <114928367+asish-sutar@users.noreply.github.com>
2024-01-11 10:18:49 +0530

    Update HotelUserDetailsService.java

--- src/main/java/com/crud/hotel/security/user/HotelUserDetailsService.java
+++ src/main/java/com/crud/hotel/security/user/HotelUserDetailsService.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 /**
- * @author Simpson Alfred
+ * @author Asish Sutar
  */
 @Service
 @RequiredArgsConstructor
